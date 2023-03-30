69fb8837215f79736d9305d1886cc7def43fa72c
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2023-03-30 19:40:55 +0530

    gtag

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,21 @@ import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
-  <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-4JHY83QRWQ"/>
-  <Script
-      id='google-analytics'
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-4JHY83QRWQ', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-    />
-  <Component {...pageProps} />
-  </>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-4JHY83QRWQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-4JHY83QRWQ');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
   )
 }
