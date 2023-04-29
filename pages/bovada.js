167d704eee8b9d5beb95913bf0f37b39f4233dcd
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2023-04-29 21:03:55 +0530

    bovada page added

---
+++ pages/bovada.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+function bovada() {
+	return (
+		<>
+			<Head>
+				<meta
+					http-equiv="refresh"
+					content="1; url= https://bit.ly/BoomBV"
+				/>
+			</Head>
+		</>
+	);
+}
+
+export default bovada;
