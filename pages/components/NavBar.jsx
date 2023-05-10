89a57622b86f944714bc917ba4b5a4be233b9d21
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2023-05-10 15:12:50 +0530

    logo redirect

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -1,17 +1,19 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function NavBar({ user, scale, setScale }) {
     return (
 
         <header>
-            <nav className="flex items-center justify-between flex-wrap p-6 400">
-                <div className="flex items-center flex-1 justify-between ">
-                    <div className='relative h-[8vh] md:w-[8.5vw] w-[13vh]'>
-                        <Image src="/logo.svg" alt="logo" fill className="mr-12" />
+            <Link href={"/"}>
+                <nav className="flex items-center justify-between flex-wrap p-6 400">
+                    <div className="flex items-center flex-1 justify-between ">
+                        <div className='relative h-[8vh] md:w-[8.5vw] w-[13vh]'>
+                            <Image src="/logo.svg" alt="logo" fill className="mr-12" />
+                        </div>
                     </div>
-                </div>
-
-            </nav>
+                </nav>
+            </Link>
         </header >
 
 
